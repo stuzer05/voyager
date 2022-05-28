dffafd808c4e273e24ecbd5efaddd8959449b57f
stuzer05 <stuzer05@gmail.com>
2022-05-28 15:41:32 +0300

    Fix missing code

--- resources/views/bread/edit-add.blade.php
+++ resources/views/bread/edit-add.blade.php
@@ -118,6 +118,7 @@
         </div>
     </div>
 
+    <!-- Delete File Modal -->
     <div class="modal fade modal-danger" id="confirm_delete_modal">
         <div class="modal-dialog">
             <div class="modal-content">
@@ -140,6 +141,30 @@
         </div>
     </div>
     <!-- End Delete File Modal -->
+
+    <!-- Delete All File Modal -->
+    <div class="modal fade modal-danger" id="confirm_delete_all_modal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"
+                            aria-hidden="true">&times;</button>
+                    <h4 class="modal-title"><i class="voyager-warning"></i> {{ __('voyager::generic.are_you_sure') }}</h4>
+                </div>
+
+                <div class="modal-body">
+                    <h4>{{ __('voyager::generic.are_you_sure_delete_all') }}</h4>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">{{ __('voyager::generic.cancel') }}</button>
+                    <button type="button" class="btn btn-danger" id="confirm_delete_all">{{ __('voyager::generic.delete_confirm') }}</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Delete All File Modal -->
 @stop
 
 @section('javascript')
@@ -252,7 +277,7 @@
                 $('#confirm_delete_all_modal').modal('hide');
             });
 
-            $('.multiple-images-sortable').sortable(p{
+            $('.multiple-images-sortable').sortable({
                 update: function(event, ui) {
                     const id = $(event.target).attr('data-id');
                     const field = $(event.target).attr('data-field-name');
